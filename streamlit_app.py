0aea99d577a7777b6a59d46ae683866874005c6b
satryaartha <satryaartha123@gmail.com>
2026-06-13 22:22:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
-import sklearn
-st.sidebar.caption(f"sklearn: {sklearn.__version__}")
 import os
 import plotly.graph_objects as go
 import plotly.express as px
